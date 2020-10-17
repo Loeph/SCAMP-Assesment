0c98a15c7783d375ec4f7643801fc024d5df9510
Loeph <66271136+Loeph@users.noreply.github.com>
2020-10-16 17:17:55 -0700

    Add files via upload

---
+++ Tolulope Mosuro Class Assignment.py
@@ -0,0 +1,10 @@
+class intro:
+    def __init__(first_parameter,name,age):
+        first_parameter.name= name
+        first_parameter.age= age
+    def a_function(cake):
+        print("Hi, my name is "+cake.name+", "+"I am "+str(cake.age)+" years old")
+
+x=intro("Tolu",23)
+x.a_function()
+    
